ddbaa1b09db2eb82e5ceb7a5323381e82566f177
Douniaalx <almansouri.dounia@gmail.com>
2023-03-17 14:02:47 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - is the main function
+ * Description: "7-print_tebahpla.c"
+ * Return: always 0
+*/
+int main(void)
+{
+int n = 122;
+	while (n >= 97)
+	{
+	putchar(n);
+	n++;
+	}
+	putchar('\n');
+	return (0);
+}
