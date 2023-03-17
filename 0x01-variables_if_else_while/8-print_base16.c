312dc0556a094301141bf3998fd918c51ee83f03
Douniaalx <almansouri.dounia@gmail.com>
2023-03-17 14:14:59 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ * main - is the main function
+ * Description: ""
+ * Return: always 0
+*/
+int main(void)
+{
+int n = 30;
+int m = 61;
+	while (n <= 39)
+	{
+	putchar(n);
+	n++;
+	}
+	while (m <= 66)
+	{
+	putchar(m);
+	m++;
+	}
+	putchar('\n');
+	return (0);
+}
