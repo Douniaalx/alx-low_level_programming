84fd96a16c325ce7d52335b340fb10b1886d50ca
Douniaalx <almansouri.dounia@gmail.com>
2023-03-21 07:42:38 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,21 @@
-#include<stdio.h>
+#include "main.h"
 /**
- * main - is the main function
+ * main - main function
+ * _putchar -  prints the alphabets putchar
  * Description: "_putchar"
- * Return: 0 means success
+ * Return: always 0
 */
+
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
